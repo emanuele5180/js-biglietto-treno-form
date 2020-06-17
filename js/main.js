08d2e76e7cc584cf4565593c0b5c3b38f992def6
emanuele5180 <macina5180>
2020-06-17 16:47:17 +0200

    errore riga28

--- js/main.js
+++ js/main.js
@@ -2,13 +2,31 @@
 var nomePasseggero = document.getElementById('nome');
 var kmDaPercorrere = document.getElementById('kmdapercorrere');
 var categoria = document.getElementById('categoria');
-//calcolo il prezzo totale del viaggio
-var costoTotale = kmDaPercorrere * 0.21;
-.console.log(costoTotale);
+
+//calcolo il prezzo del viaggio
+var costo = kmDaPercorrere * 0.21;
 
 //il prezzo del biglietto è definito in base ai km (0.21 € al km)
 //va applicato uno sconto del 20% per i minorenni
-var scontoMinorenni = ((costoTotale * 20) / 100);
+//var scontoMinorenni = ((costo * 20) / 100);
 
 //va applicato uno sconto del 40% per gli over 65.
-var scontoOver65 = ((costoTotale*40) / 100);
+//var scontoOver65 = ((costo*40) / 100);
+
+//calcolo il prezzo totale del viaggio
+
+if (scontoMinorenni) {
+  var scontoMinorenni = ((costo * 20) / 100);
+
+}
+
+else if (scontoOver65) {
+  var scontoOver65 = ((costo*40) / 100);
+
+}
+
+else (costo) {
+  var costo = kmDaPercorrere * 0.21;
+
+}
+console.log (costo) ;
