8b1708eeb9f48cddafd5761b6c079452ae7e646e
emanuele5180 <macina5180>
2020-06-17 17:52:33 +0200

    listener

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 //l'inserimento del nome del passeggero, dei chilometri che vuole percorrere e la selezione di una categoria (over65, minorenne, maggiorenne).
-var nomePasseggero = document.getElementById('nome');
-var kmDaPercorrere = document.getElementById('kmdapercorrere');
-var categoria = document.getElementById('categoria');
+var submit = document.getElementById('submit');
+
+submit .addEventListener ( "click", function () {
+var nomePasseggero = document.getElementById('nome').value;
+var kmDaPercorrere = document.getElementById('kmdapercorrere').value;
+var categoria = document.getElementById('categoria').value;
 
 //calcolo il prezzo del viaggio
 var costo = kmDaPercorrere * 0.21;
 
 //il prezzo del biglietto è definito in base ai km (0.21 € al km)
 //va applicato uno sconto del 20% per i minorenni
-//var scontoMinorenni = ((costo * 20) / 100);
-
 //va applicato uno sconto del 40% per gli over 65.
-//var scontoOver65 = ((costo*40) / 100);
-
 //calcolo il prezzo totale del viaggio
 
-if (scontoMinorenni) {
+if (categoria == 'minorenne') {
   var scontoMinorenni = ((costo * 20) / 100);
+  costo = costo - scontoMinorenni;
 
-}
-
-else if (scontoOver65) {
+}else if (categoria == 'over65') {
   var scontoOver65 = ((costo*40) / 100);
-
-}
-
-else (costo) {
+  costo = costo - scontoOver65;
+}else {
   var costo = kmDaPercorrere * 0.21;
-
 }
+
+
+
 console.log (costo) ;
+})
